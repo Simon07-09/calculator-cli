defc76245510330720ea5012a7d6e4c4bf0e9664
Zenkreii <116511164+Zenkreii@users.noreply.github.com>
2024-08-01 12:24:56 -0500

    update of code

--- calculator/cli.py
+++ calculator/cli.py
@@ -1,4 +1,4 @@
-import click 
+import click
 
 
 from calculator.model import Calculator
@@ -8,6 +8,57 @@ from calculator.model import Calculator
 @click.group()
 @click.pass_context
 def calc(ctx: click.Context):
-    """A simple calculator"""
+    """ A simple calculator"""
+
+    ctx.obj = {"calculator_object", Calculator()}
+
+
+@click.command()
+@click.argument('a', type=float)
+@click.argument('b', type=float)
+@click.pass_context
+def add(ctx: click.Context, a: float, b: float) -> None:
+    """Add two numbers."""
+    calculator = ctx.obj["calculator_object"]
+    result = calculator.add(a, b)
+    click.echo(result)
+
+
+@click.command()
+@click.argument('a', type=float)
+@click.argument('b', type=float)
+@click.pass_context
+def subtract(ctx: click.Context, a: float, b: float) -> None:
+    """Add two numbers."""
+    calculator = ctx.obj["calculator_object"]
+    result = calculator.subtract(a, b)
+    click.echo(result)
+
+
+
+@click.command()
+@click.argument('a', type=float)
+@click.argument('b', type=float)
+@click.pass_context
+def multiply(ctx: click.Context, a: float, b: float) -> None:
+    """Multiply two numbers."""
+    calculator = ctx.obj["calculator_object"]
+    result = calculator.multiply(a, b)
+    click.echo(result)
     
-    ctx.obj = {"calculator_object", Calculator{}}
+
+@click.command()
+@click.argument('a', type=float)
+@click.argument('b', type=float)
+@click.pass_context
+def divide(ctx: click.Context, a: float, b: float) -> None:
+    """Divide two numbers."""
+    calculator = ctx.obj["calculator_object"]
+    result = calculator.divide(a, b)
+    click.echo(result)
+    
+
+calc.add_command(add)
+calc.add_command(subtract)
+calc.add_command(multiply)
+calc.add_command(divide)
